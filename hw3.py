2b537eb1aba86e792a60fba891c0489d208dd65e
Christopher Fu <cfu6@binghamton.edu>
2018-03-25 00:00:25 -0400

    finished based code,  debugging

--- hw3.py
+++ hw3.py
@@ -1,6 +1,7 @@
 from perceptron import Perceptron
-import argparse, os
+from nltk.stem.porter import * 
 from collections import Counter
+import argparse, os, re
 
 def getArgs():
     p = argparse.ArgumentParser(
@@ -11,6 +12,20 @@ def getArgs():
     p.add_argument('spam', 
             help='An already stemmed text file containing all of the words \
             in all the SPAM emails in the TRAINING set')
+    p.add_argument('trainHamDir', 
+            help='The directory where all the HAM emails in the TEST set \
+                    are located')
+    p.add_argument('trainSpamDir', 
+            help='The directory where all the SPAM emails in the TEST set \
+            are located')
+    p.add_argument('testHamDir', 
+            help='The directory where all the HAM emails in the TEST set \
+                    are located')
+    p.add_argument('testSpamDir', 
+            help='The directory where all the SPAM emails in the TEST set \
+            are located')
+    p.add_argument('stopWords',nargs='?',default ='', 
+            help='(optional) file of stopwords to ignore when stemming')
     return p.parse_args()
 
 def initBagFromList(docList):
@@ -37,7 +52,7 @@ def initBagFromFile(st):
             c.update(l)
     return c
 
-def stemDoc(self, docPath,stopWords=""):
+def stemDoc(docPath,stopWords=""):
     stopWordsL = []
     if stopWords != "":
         with open(stopWords,'r', encoding='utf-8', errors='ignore') as stopFile:
@@ -58,16 +73,56 @@ def stemDoc(self, docPath,stopWords=""):
     stemmed_words_list = [stemmer.stem(plural) for plural in unstemmed_words_list]
     return stemmed_words_list
 
+def trainWithFiles(p, target, dir):
+    ''' (perceptron object, string) -> None
+        
+        Given an perceptron object and a directory path where the emails are, train
+        the perceptron with emails. Target is 1 if emails are ham, -1 if they are spam.
+    '''
+    for doc in os.listdir(dir):
+        stemmedDoc = stemDoc(dir+doc)
+        bag = initBagFromList(stemmedDoc)
+        p.train(target,bag)
+
+def testWithFiles(p, target, dir):
+    total = 0
+    numCorrect = 0
+    for doc in os.listdir(dir):
+        total += 1
+        stemmedDoc = stemDoc(dir+doc)
+        bag = initBagFromList(stemmedDoc)
+        ans =  p.classify(bag) 
+        #print("{}=?={}".format(ans,target))
+        if ans == target:
+            numCorrect += 1
+    print("{} correct out of {}".format(numCorrect, total))        
+    return (numCorrect,total)
+
 if __name__ == "__main__":
     args = getArgs()
     ham = args.ham
     spam = args.spam
+    trainHamDir = args.trainHamDir + '/' if (args.trainHamDir[-1] != '/') else args.trainHamDir
+    trainSpamDir = args.trainSpamDir + '/' if (args.trainSpamDir[-1] != '/') else args.trainSpamDir
+    testHamDir = args.testHamDir + '/' if (args.testHamDir[-1] != '/') else args.testHamDir
+    testSpamDir = args.testSpamDir + '/' if (args.testSpamDir[-1] != '/') else args.testSpamDir
+    stopWords = args.stopWords
 
     hamBag = initBagFromFile(ham) # class 1
-    spamBag = initBagFromFile(spam) # class 0
+    spamBag = initBagFromFile(spam) # class -1
     mergedBag = hamBag + spamBag
 
-    print(hamBag)
-
     p = Perceptron(.1)
-    
+    p.initFeatures(mergedBag)
+    p.initWeights()
+
+    print("------------TRAINING HAM------------")
+    trainWithFiles(p, 1, trainHamDir)
+    #print("\n----------TRAINING SPAM-----------")
+    #trainWithFiles(p, -1, trainSpamDir)
+
+    #print("\n------------TESTING HAM--------------")
+    #hamTest = testWithFiles(p, 1, testHamDir)
+    #print("\n-----------TESTING SPAM--------------")
+    #spamTest = testWithFiles(p, -1, testSpamDir)
+    #print("PERCENT CORRECT {}".format((hamTest[0]+spamTest[0])/(hamTest[1]+spamTest[1])))
--- perceptron.py
+++ perceptron.py
@@ -1,54 +1,58 @@
 class Perceptron():
 
     # Since we need to correlate weights and inputs to specific words, 
-    # we will use a dict for the weights instead of a vector
+    # we will use a counter for the weights instead of a vector
     def __init__(self, learningRate, featureVectorIn={}):
-        self.featureVector = featureVectorIn # dict
-        self.weightVector = {} # dict
+        self.featureVector = featureVectorIn # counter
+        self.weightVector = {} # counter
         self.n = learningRate
 
     # Need to init perceptron with set of attributes and their freq from ALL of the emails (spam and ham)
     def initFeatures(self,trainedBoW):
         self.featureVector = trainedBoW
-        self.featureVector["BIAS"] = 1 # setting a W0 bias
+        self.featureVector["__BIAS__"] = 1 # setting a W0 bias
     
     # Initialize weights to size of input vector, init to 0 instead of random
     def initWeights(self):
-        self.weightVector = {x:0 for x in featureVector.keys()}
+        self.weightVector = {x:0 for x in self.featureVector.keys()}
+        self.weightVector["__BIAS__"] = 1 #need to ensure always == 1
 
-    #given an input attr dict from a test email, classify it 
+    #given an input attr counter from a test email, classify it 
     def classify(self, inputDict):
         wxArr = []
         for key in inputDict.keys():
-            wxArr.append(featureVector[key]*weightVector[key])
+            try:
+                wxArr.append(self.featureVector[key]*self.weightVector[key])
+                #print("1", end = "")
+            except KeyError as e:
+                # Note: KeyErrors should be normal during TESTING but not TRAINING
+                #print("KEY {} not found".format(e))            
+                #print("\300[91m 0 \033[0m", end = "")
+                pass
+        #Should be able to just replace with 1
+        wxArr.append(1)
         res = sum(wxArr)
-        return 1 if res >1 else 0
+        print("{} = {}".format(wxArr,res))
+        #print("{:.3f}".format(res))
+        return 1 if res>0 else -1
            
     def train(self, target, inputDict):
-        '''(int, dict) -> None
+        '''(int, counter) -> None
             Trains the perceptron on a new single email, provided as an inputDict.
 
-            Takes in target (class value, so 1 for ham and 0 for spam) and
-            dictionary of a single training email and trains the perceptron 
+            Takes in target (class value, so 1 for ham and -1 for spam) and
+            counter of a single training email and trains the perceptron 
             using the training rule wi <- wi + delta(wi), where delta(wi) 
             = n(t-o)xi. n is learning rate, (t-o) is the difference between
             perceptron c(vector(X)) output and target value, and xi is the specific attr
             we are updating the weight wi for.
         '''
-        o = classify(inputDict) # pre-calc, doesn't change per email
+        o = self.classify(inputDict) # pre-calc, doesn't change per email
         for key in inputDict.keys():
             try:
-                deltaWI = self.n * ( target - o ) * featureVector[key]
-                weightVector[key] += deltaWI
+                deltaWI = self.n * ( target - o ) * self.featureVector[key]
+                self.weightVector[key] += deltaWI
             except KeyError as e:
                 #key not found? Should not happen since Perceptron attr are set before training
-                print("KEY {} not found".format(e))            
-        
-        #for doc in os.listdir(dir):
-        #    stemmedDoc = stemDoc(dir+doc)
-        #    bag = initBagFromList(stemmedDoc)
-        #    for key,value in bag.items():
-        #        if key in self.featureVector:
-                    # w inc by 1
-        #        else:
-                    # add entry in fV, set w to 1
+                #print("KEY {} not found".format(e))            
+                pass 
